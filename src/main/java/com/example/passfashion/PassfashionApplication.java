8d24828d70862920774c29609d3dd14e43374b40
tonhat211 <21130463@st.hcmuaf.edu.vn>
2025-05-14 19:10:17 +0700

    init spring boot project

---
+++ src/main/java/com/example/passfashion/PassfashionApplication.java
@@ -0,0 +1,13 @@
+package com.example.passfashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassfashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassfashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/passfashion/PassfashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passfashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassfashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
