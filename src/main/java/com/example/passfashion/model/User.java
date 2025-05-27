efd057aee11bf1fb0508d5b6bf474460a2d9fb74
NLURAT <21130577@st.hcmuaf.edu.vn>
2025-05-27 13:08:40 +0700

    Cập nhật xử lý Đăng nhập, đăng ký

--- src/main/java/com/example/passfashion/model/User.java
+++ src/main/java/com/example/passfashion/model/User.java
@@ -1,34 +1,30 @@
 package com.example.passfashion.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
-import lombok.Data;
+import java.util.UUID;
 
 @Entity
 @Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.JOINED)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = true)
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -44,19 +40,17 @@ public class User {
     @Column(name = "birthday", nullable = true)
     private LocalDate birthday;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private List<WishList> wishlist;
+    @Column(name = "phone", nullable = true)
+    private String phone;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Address> addresses = new ArrayList<>();
 
-    @Column(name = "saled_order_qty", nullable = false, columnDefinition = "INT DEFAULT 0")
-    private int saledOrderQty;
-    @Column(name = "phone", nullable = true)
-    private String phone;
+    @Column(name = "sold_order_qty", nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int soldOrderQty;
 
-    @Column(name = "rating", nullable = false, columnDefinition = "DOUBLE DEFAULT 0")
-    private double rating;
+    @Column(name = "rating", nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int rating;
 
     @Column(name = "total_review", nullable = false, columnDefinition = "INT DEFAULT 0")
     private int totalReview;
@@ -67,4 +61,10 @@ public class User {
     @Transient
     private String avatar;
 
+    @PrePersist
+    public void prePersist() {
+        if (this.name == null || this.name.trim().isEmpty()) {
+            this.name = "user" + UUID.randomUUID().toString().substring(0, 8);
+        }
+    }
 }
