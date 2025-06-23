9aa4ca787df206fdb566bf280d48764dd8c46b9b
tonhat211 <21130463@st.hcmuaf.edu.vn>
2025-06-23 10:15:26 +0700

    be admin product

--- src/main/java/com/example/passfashion/config/SecurityConfig.java
+++ src/main/java/com/example/passfashion/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/ad/**").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/api/v1/products/create").authenticated()
                         .requestMatchers("/api/v1/products/material").authenticated()
